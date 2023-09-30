378f160a13fa793d11fef974fbc005a53f298d75
Nathan van Deventer <n.vandeventer@student.maastrichtuniversity.nl>
2023-09-30 14:42:28 +0200

    start adjusting transitions with matrix

--- Game.java
+++ Game.java
@@ -33,6 +33,38 @@ public class Game{
 		// Printing the opening dialogue
 		printState(stateId);
 
+		String[] storyArray = {"You are standing in an abandoned university office.\nThere are neither students "
+				+ "nor teachers around you.\nThere's a table in front of you with various papers, "
+				+ "pens, a small puzzle toy, and a calculator.\n"
+				+ "A large window shows an empty office building; there are no Zombies in the empty "
+				+ "building (as far as you can tell).\nBehind you is a dark and mysterious door that "
+				+ "leads to a well-lit corridor with a fireproof ceiling and floor.\nYou feel a "
+				+ "sense of Wi-Fi around you, the grinding of an LCD operated coffee machine can be "
+				+ "heard in the distance.\nYou are not thirsty, but you rather have a craving for "
+				+ "justice.",
+				"You are in a long hallway.\nThere\'s a man wearing glasses at the end of it, he "
+				+ "looks harmless.\nWest is a wall, east is the man, to the north is nothing but "
+				+ "empty offices, a desperate sight.\nThe carpeting in the hallway feels soft, you "
+				+ "hear the clicking of a mouse in the distance.\nYour office is south (behind "
+				+ "you).",
+				"You take the calculator from your desk.\nIt's a Casio FX-85gt Plus.\nThe "
+				+ "display shows the number 0.1134.\nYou turn it upside down; now the Casio "
+				+ "greets you with a friendly “hello”, nice.\nYou hold the calculator in your hand.",
+				"The man greets you and starts endlessly talking to you about his children "
+				+ "and his holiday to Benidorm.\nYou die of boredom.",
+				"You enter the hallway with the Casio FX-85gt stand-by. Having this small device "
+				+ "greet you puts you in a good mood, somehow the building feels less lonely than "
+				+ "before. West is a wall, looking east you stare into the darkness, the corridor is too "
+				+ "long to see the end. To the north you see an office with what looks like a small "
+				+ "creature in a corner. The carpeting in the hallway feels soft, you hear someone "
+				+ "explaining algorithms to your north.",
+				"You enter the office. To your surprise a small dog is sitting in the corner. Surely this "
+				+ "breaks any number of university regulations! In a high-pitched voice the dog tells "
+				+ "you how to write even more cool words on your Casio FX-85gt, good boi! To the "
+				+ "north you see an open window, a ladder hangs down from it, it looks so dangerous! "
+				+ "An LCD display shows a youtube video about developing algorithms."
+				};
+
 		while (stateId != 666){
 			String input = getInput();
 			takeAction(input, stateId);
@@ -87,24 +119,30 @@ public class Game{
 	}
 
 	public static void takeAction(String action, int currentState){
-		switch(action){
-			case "open the door": 
-				if (currentState == 0){
-					stateId = 1;
-				}
-				break;
-			case "take item":
-				if (currentState == 0){
-					stateId = 2;
-				}
-				break;
-			case "go east":
-				if (currentState == 1){
-					stateId = 3;
-				}
-				break;
-			case "exit":
-			case "quit": stateId = 666; break;
+		
+		String[][] transitionMatrix = new String[6][6];
+		for (int i = 0; i < transitionMatrix.length; i++){
+			for (int c = 0; c < transitionMatrix.length; c++){
+				transitionMatrix[i][c] = "";
+			}
+		}
+
+		transitionMatrix[0][1] = "open the door";
+		transitionMatrix[0][2] = "take item";
+		transitionMatrix[1][0] = "go south";
+		transitionMatrix[1][3] = "go east";
+		transitionMatrix[2][4] = "open the door";
+		transitionMatrix[4][1] = "drop item";
+		transitionMatrix[4][5] = "go north";
+		transitionMatrix[5][1] = "go south";
+
+		for (int i = 0; i < transitionMatrix.length; i++){
+			if (transitionMatrix[currentState][i].equals(action)){
+				stateId = i;
+			}
+			else if (action.equalsIgnoreCase("quit")){
+				stateId = 666;
+			}
 		}
 	}
 
@@ -141,5 +179,9 @@ public class Game{
 				break;
 		}
 		System.out.println("\n" + story + "\n");
+	}
+
+	public static String getStory(int stateId, String[] storyArray){
+
 	}
 }
